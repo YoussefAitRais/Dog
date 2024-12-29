e7230a3018e1ba4f9c95d7cb6d010607107e3b2d
YoussefAitRais <copybuisness007@gmail.com>
2024-12-29 20:56:57 +0100

    dog

---
+++ src/Dog.java
@@ -0,0 +1,29 @@
+
+public class Dog {
+
+    private String name;
+    private String race;
+
+
+    public Dog(String name, String race) {
+        this.name = name;
+        this.race = race;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                Dog dog1 = new Dog("ricks", "Chienpolice");
+                Dog dog2 = new Dog("jack", "Pitbul");
+
+
+                dog1.setName("ricks");
+                dog1.setRace("Chienpolice");
+
+                dog2.setName("jack");
+                dog2.setRace("Pitbul");
+
+
+
+
+                System.out.println("Name of Dog1 " + dog1.getName());
+                System.out.println("Name of Dog2 " + dog2.getName());
+
+                System.out.println("-------------------------------");
+
+                System.out.println("Race of Dog1 " + dog1.getRace());
+                System.out.println("Race of Dog2 " + dog2.getRace());
+
+
+    }
+}
